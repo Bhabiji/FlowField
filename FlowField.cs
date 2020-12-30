4e952cd9748024008e4e867556587b7680d25de9
Bhabiji <caerelsjas@gmail.com>
2020-12-30 21:42:44 +0100

    Implemented integration field algorithm
    Implementation of integration algorithm (actual pathfinding)
    Changed node struct into class for more flexible value changing during list iterations (Integrationfield)
    GetNodeIdxFromWorld implemented (inspiration from Elite framework GPP)
    Extra debug functionality

--- FlowField.cs
+++ FlowField.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct Node
+public class Node
 {
     public Vector3 pos;
     public int row;
     public int col;
     public int travelCost;
-    public int cellValue;
+    public int nodeValue;
+
     public Node(Vector3 newPos, int currRow, int currCol, int currTravelCost)
         {
             pos = newPos;
             row = currRow;
             col = currCol;
             travelCost = currTravelCost;
+            nodeValue = 65535;
         }
 
 }
@@ -41,13 +43,13 @@ public class FlowField : MonoBehaviour
         m_GridSize = m_Plane.GetComponent<Renderer>().bounds.size;
         //generate grid over whole plane dependant on nodeSize
         //-1 to def clamp withing plane
-        m_AmountOfRows = (int)(m_GridSize.z / m_NodeSize) - 1;
-        m_AmountOfCols = (int)(m_GridSize.x / m_NodeSize) - 1;
-        Debug.Log(m_GridSize.z);
-        Debug.Log(m_GridSize.x);
-        Debug.Log(m_AmountOfRows);
+        m_AmountOfRows = (int)(m_GridSize.z / m_NodeSize);
+        m_AmountOfCols = (int)(m_GridSize.x / m_NodeSize);
+        //Debug.Log(m_GridSize.z);
+        //Debug.Log(m_GridSize.x);
+        //Debug.Log(m_AmountOfRows);
 
-        Debug.Log(m_AmountOfCols);
+        //Debug.Log(m_AmountOfCols);
 
         m_FlowFieldGrid = new Node[m_AmountOfRows, m_AmountOfCols];
 
@@ -55,23 +57,55 @@ public class FlowField : MonoBehaviour
         InitGrid();
         InitCostField();
     }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            InitGrid();
+            InitCostField();
+            HandleInput();
+
+            for (int i = 0; i < m_AmountOfRows; i++)
+            {
+                for (int j = 0; j < m_AmountOfCols; j++)
+                {
+                    
+                    //Debug.Log(m_FlowFieldGrid[i, j].nodeValue);
+                }
+            }
+
+        }
+    }
+
+    void HandleInput()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+        //Debug.Log(mousePos.x);
+        //Debug.Log(mousePos.z);
+
+        Vector2Int endNodeIdx = GetNodeIdxFromWorldPos(mousePos);
+        m_EndNode = m_FlowFieldGrid[endNodeIdx.x, endNodeIdx.y];
+        InitIntegrationField();
+    }
+
     private void InitGrid()
     {
         for (int i = 0; i < m_AmountOfRows; i++)
         {
             for (int j = 0; j < m_AmountOfCols; j++)
             {
-                Vector3 currNodePos = new Vector3((-m_GridSize.x / 2) + (m_NodeSize * j + m_NodeSize / 2), 0, (-m_GridSize.z / 2) + (m_NodeSize * i + m_NodeSize / 2)); //extra offset added so cells dont overlap
+                Vector3 currNodePos = new Vector3((-m_GridSize.x / 2 + m_NodeSize / 2) + (m_NodeSize * j + m_NodeSize / 2), 0, (-m_GridSize.z / 2 + m_NodeSize / 2) + (m_NodeSize * i + m_NodeSize / 2)); //extra offset added so Nodes dont overlap
                 m_FlowFieldGrid[i, j] = new Node(currNodePos, i, j, 1);
-                Debug.Log("Node created with index");
-                Debug.Log(i);
-                Debug.Log(j);
-                Debug.Log("On pos:");
-                Debug.Log(currNodePos);
+                //Debug.Log("Node created with index");
+                //Debug.Log(i);
+                //Debug.Log(j);
+                //Debug.Log("On pos:");
+                //Debug.Log(currNodePos);
 
             }
         }
-        Debug.Log("Grid Created");
+        //Debug.Log("Grid Created");
     }
 
     //https://leifnode.com/2013/12/flow-field-pathfinding/
@@ -98,19 +132,19 @@ public class FlowField : MonoBehaviour
                     {
                         //Mud == slightly higher cost
                         m_FlowFieldGrid[i, j].travelCost = 5;
-                        Debug.Log("Mud");
+                        //Debug.Log("Mud");
                     }
                     else if (collider.gameObject.layer == 4)
                     {
                         //Water == high cost
                         m_FlowFieldGrid[i, j].travelCost = 10;
-                        Debug.Log("Water");
+                        //Debug.Log("Water");
                     }
                     else if (collider.gameObject.layer == 9)
                     {
                         //Obstacle cant walk through
                         m_FlowFieldGrid[i, j].travelCost = 255;
-                        Debug.Log("Obstacle");
+                        //Debug.Log("Obstacle");
                     }
 
                 }
@@ -139,23 +173,34 @@ public class FlowField : MonoBehaviour
 
     //IntegrationField
     //https://leifnode.com/2013/12/flow-field-pathfinding/
-    public void InitIntegrationField()
+    private void InitIntegrationField()
     {
         //List<Node> openList = new List<Node>();
         //openList.Add(m_EndNode);
 
+        //A* reset for end node to start the loop
+        m_EndNode.nodeValue = 0;
+        m_EndNode.travelCost = 0;
+
         Queue<Node> openList = new Queue<Node>();
         openList.Enqueue(m_EndNode);
+
         List<Node> connectedNodes;
-        
+
         while (openList.Count != 0)
         {
-            //get last cell in queue
+            //get last node in queue, throw that node out of queue 
             Node currNode = openList.Dequeue();
             connectedNodes = GetConnectedNodes(currNode, false);
             for (int i = 0; i < connectedNodes.Count; i++)
             {
-                
+                int gCost = connectedNodes[i].travelCost + currNode.nodeValue;
+                //This will generate the distance values for each node from the end node  and be 65535 for obstacles
+                if (gCost < connectedNodes[i].nodeValue)
+                {
+                    connectedNodes[i].nodeValue = gCost;
+                    openList.Enqueue(connectedNodes[i]);
+                }
             }
         }
     }
@@ -190,17 +235,35 @@ public class FlowField : MonoBehaviour
             Vector2Int connectedNodeIdx = currIdx + connections[i];
 
             //check if node is outside grid, if so, skip this direction
-            if(connectedNodeIdx.x < 0 || connectedNodeIdx.y < 0 || connectedNodeIdx.x > m_AmountOfRows || connectedNodeIdx.y > m_AmountOfCols)
+            if(connectedNodeIdx.x < 0 || connectedNodeIdx.y < 0 || connectedNodeIdx.x >= m_AmountOfRows || connectedNodeIdx.y >= m_AmountOfCols)
             {
                 continue;
             }
-            connectedNodes.Add(m_FlowFieldGrid[connectedNodeIdx.x, connectedNodeIdx.y]);
+            else
+            {
+                connectedNodes.Add(m_FlowFieldGrid[connectedNodeIdx.x, connectedNodeIdx.y]);
+            }
         }
+        
 
         return connectedNodes;
     }
 
+    private Vector2Int GetNodeIdxFromWorldPos(Vector3 pos)
+    {
+        //Add offset to convert 0,0 origin to leftBottom of gameObject
+        pos.x += m_GridSize.x / 2;
+        pos.z += m_GridSize.z / 2;
 
+        Debug.Log(Mathf.FloorToInt(0.5f));
+
+        float xRatio = Mathf.Clamp(pos.x / (m_AmountOfCols * m_NodeSize), 0,1);
+        float yRatio = Mathf.Clamp(pos.z / (m_AmountOfRows * m_NodeSize), 0, 1); //If u think traditional 2d even tho working in z-axis
+
+        int x = Mathf.Clamp((int)((m_AmountOfRows) * yRatio), 0, m_AmountOfRows - 1);
+        int y = Mathf.Clamp((int)((m_AmountOfCols) * xRatio), 0, m_AmountOfCols - 1);
+        return new Vector2Int(x,y);
+    }
 
     //------------------------DEBUGGING---------------------------
     private void OnDrawGizmos()
@@ -212,21 +275,22 @@ public class FlowField : MonoBehaviour
             {
                 for (int j = 0; j < m_AmountOfCols; j++)
                 {
-                    Vector3 currNodePos = new Vector3((-m_GridSize.x/2 + m_NodeSize/2) + (m_NodeSize * j + m_NodeSize / 2), 0, (-m_GridSize.z/2 + m_NodeSize / 2) + (m_NodeSize * i + m_NodeSize / 2)); //extra offset added so cells dont overlap
+                    Vector3 currNodePos = m_FlowFieldGrid[i, j].pos;
                     Vector3 nodeSize = Vector3.one * m_NodeSize * 2;
 
-                    if(m_FlowFieldGrid[i,j].travelCost == 1)
-                        Gizmos.color = Color.blue;
+                    //if(m_FlowFieldGrid[i,j].travelCost == 1)
+                    //    Gizmos.color = Color.blue;
 
-                    else if (m_FlowFieldGrid[i, j].travelCost == 5)
-                        Gizmos.color = Color.green;
+                    //else if (m_FlowFieldGrid[i, j].travelCost == 5)
+                    //    Gizmos.color = Color.green;
 
-                    else if (m_FlowFieldGrid[i, j].travelCost == 10)
-                        Gizmos.color = Color.yellow;
+                    //else if (m_FlowFieldGrid[i, j].travelCost == 10)
+                    //    Gizmos.color = Color.yellow;
 
-                    else if (m_FlowFieldGrid[i, j].travelCost == byte.MaxValue)
-                        Gizmos.color = Color.black;
-
+                    //else if (m_FlowFieldGrid[i, j].travelCost == byte.MaxValue)
+                    //    Gizmos.color = Color.black;
+                    float currNodeValue = m_FlowFieldGrid[i, j].nodeValue;
+                    Gizmos.color = new Color(currNodeValue / 10, currNodeValue / 10, currNodeValue / 10);
                     Gizmos.DrawWireCube(currNodePos, nodeSize);
                 }
             }
